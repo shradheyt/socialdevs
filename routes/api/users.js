46571ca28e16871da41198e854fa14411078a497
Shradhey Tripathi <KH2333@kitspl.com>
2018-09-09 23:44:55 +0530

    Added login module with web tokens

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User')
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -43,7 +46,55 @@ router.post('/register', (req, res) => {
                 })
             })
         }
-    })
-})
+    });
+});
+
+// @route   POST api/users/login
+// @desc    Login User / returning token
+// @access  Public
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find user by email
+    User.findOne({email})
+    .then(user => {
+        //Check for user
+        if(!user) {
+            return res.status(404).json({email: 'User not found'});
+        }
+
+        //Check Password
+        bcrypt.compare(password, user.password)
+        .then(isMatch => {
+            if(isMatch) {
+                //User matched
+                const payload = { id: user.id, name: user.name, avatar: user.avatar }; //Create JWT payload
+                //Sign Token
+                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: 'Bearer ' + token
+                    })
+                });
+            } else {
+                return res.status(400).json({password: 'Password incorrect'});
+            }
+        });
+    });
+});
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({ 
+        id:req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
+});
 
 module.exports = router;
