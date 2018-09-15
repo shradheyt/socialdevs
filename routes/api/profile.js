6d197b7fd395e99a9c50b11fe0310196af1ae48e
Shradhey Tripathi <KH2333@kitspl.com>
2018-09-15 23:58:01 +0530

    Started /profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-// @route   GET api/profile/test
-// @desc    Tests profile route
-// @access  Public
-router.get('/test', (req, res) => { res.json({ msg: 'Profile Works' }) });
+
+//Load profile model
+const Profile = require('../../models/Profile');
+//Load user model
+const User = require('../../models/User');
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {  
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+        if(!profile) {
+            errors.noprofile = "No Profile for this user";
+            return res.status(404).json(errors);
+        }
+        res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
