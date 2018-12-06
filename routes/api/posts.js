3323c0305c3b22eff1eb9ed990ec92015f8e3198
shradheyt <shradhey029tripathi@gmail.com>
2018-12-06 22:42:21 +0530

    Added Posts model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Post = require('../../models/Post');
 
-router.get('/test', (req, res) => { res.json({ msg: 'Posts Works' }) });
+// @route   POST api/posts
+// @desc    Create post
+// @access  private
+
+router.post('/',passport.authenticate('jwt', { session: false }), (req, res) => {
+    const newPost = new Post({
+        text: req.body.text,
+        name:req.body.name,
+        avatar:req.body.name,
+        user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+})
 
 module.exports = router;
